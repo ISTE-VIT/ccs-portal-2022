60d5cc594a189ad0b61e3dedf76e8253be50cd8a
sahildotexe <sahilkaling@gmail.com>
2022-01-22 18:56:19 +0530

    total timer

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -100,7 +100,7 @@ const showQuestion = async (index) => {
             'click',
             manageCurrentNonAptitudeResponse,
           )
-          showQuestion(index + 1)
+         //  showQuestion(index + 1)
         }
         nextButton.addEventListener('click', manageCurrentNonAptitudeResponse)
       }
@@ -149,3 +149,26 @@ const finishExam = async () => {
   }
 
   window.addEventListener('DOMContentLoaded', () => {})
+
+  function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+          finishExam()
+        }
+    }, 1000);
+}
+
+window.onload = function () {
+    var fMinutes = 60 * 45,
+        display = document.querySelector('#ttime');
+    startTimer(fMinutes, display);
+};
